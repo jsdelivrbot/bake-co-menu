f6c9211978d2dc2d70223ca83cfc6a997b5159bb
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-23 17:07:56 +0100

    Store with persisted state

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import {persistStore, autoRehydrate} from 'redux-persist'
 
 import App from './components/app'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const store = createStore(reducers, undefined, autoRehydrate())
+persistStore(store)
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={ store }>
     <App />
   </Provider>
   , document.querySelector('.wrapper'))
